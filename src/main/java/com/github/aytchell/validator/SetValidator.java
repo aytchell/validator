8f001461ff272ae29156518ba105398afa879548
Hannes Lerchl <hannes.lerchl@googlemail.com>
2020-11-11 18:14:24 +0100

    Fixed typos in parameter names

--- src/main/java/com/github/aytchell/validator/SetValidator.java
+++ src/main/java/com/github/aytchell/validator/SetValidator.java
@@ -5,9 +5,9 @@ import com.github.aytchell.validator.exceptions.ValidationException;
 public interface SetValidator<E> {
     SetValidator<E> isEmpty() throws ValidationException;
 
-    SetValidator<E> containsLessThan(int minNumberOfElemens) throws ValidationException;
+    SetValidator<E> containsLessThan(int minNumberOfElements) throws ValidationException;
 
-    SetValidator<E> containsMoreThan(int maxNumberOfElemens) throws ValidationException;
+    SetValidator<E> containsMoreThan(int maxNumberOfElements) throws ValidationException;
 
     SetValidator<E> isContained(E key) throws ValidationException;
 
