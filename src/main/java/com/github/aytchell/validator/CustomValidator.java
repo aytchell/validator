b533740202cf0783d58b6597f554d47117e99cb8
Hannes Lerchl <hannes.lerchl@googlemail.com>
2020-11-28 19:23:48 +0100

    Cosmetics

--- src/main/java/com/github/aytchell/validator/CustomValidator.java
+++ src/main/java/com/github/aytchell/validator/CustomValidator.java
@@ -2,6 +2,7 @@ package com.github.aytchell.validator;
 
 import com.github.aytchell.validator.exceptions.ValidationException;
 
+@FunctionalInterface
 public interface CustomValidator<TYPE> {
     CustomValidator<TYPE> passes(CustomEntryValidator<TYPE> entryValidator) throws ValidationException;
 }
--- src/main/java/com/github/aytchell/validator/ListValidator.java
+++ src/main/java/com/github/aytchell/validator/ListValidator.java
@@ -1,6 +1,4 @@
 package com.github.aytchell.validator;
 
-import com.github.aytchell.validator.exceptions.ValidationException;
-
 public interface ListValidator<T> extends CollectionValidator<T, ListValidator<T>> {
 }
